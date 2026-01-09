39844a1f6078746f2be7585633c77042d2f86e8e
Milan Jevgenijevic <milanje@Milans-MacBook-Pro.local>
2026-01-09 09:59:33 +0100

    Fix: client can read server now

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "io"
-    "time"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
 )
 
 func main() {
-    client := &http.Client{
-        Timeout: 10 * time.Second,
-    }
-    req, err := http.NewRequest(
-        http.MethodGet,
-        "https://localhost:8080",
-        nil,
-    )
-    if err != nil {
-    }
-    resp, err := client.Do(req)
-    if err != nil {
-    }
-    defer resp.Body.Close()
-    bodyBytes, err := io.ReadAll(resp.Body)
-    if err != nil {
-    }
-    fmt.Println(string(bodyBytes))
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	req, err := http.NewRequest(
+		http.MethodGet,
+		"http://localhost:8080",
+		nil,
+	)
+	if err != nil {
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+	}
+	fmt.Println(string(bodyBytes))
 }
