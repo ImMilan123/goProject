54c595b0dfa96d5e8e3c60e990876f0eadfb7b24
Milan Jevgenijevic <milanje@Milans-MacBook-Pro.local>
2026-01-12 12:52:11 +0100

    feat: added header wich gets read and sent back to the client by the server.

--- server.go
+++ server.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Hello World :)")
-    })
-
-    fmt.Println("running")
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w,
+        r.Header)
+	})
+	fmt.Println("running")
+	http.ListenAndServe(":8081", nil)
 }
+
